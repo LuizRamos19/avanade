dceaf08fe7d8ac6246a2af2030a2f15eb3236062
Douglas Moura <douglas.ademoura@gmail.com>
2019-06-29 16:06:16 -0300

    Diminuindo o código

--- githubwars/index.js
+++ githubwars/index.js
@@ -1,27 +1,14 @@
-function getGitInformation(username) {
-  let request = `https://api.github.com/users/${username}`;
-  let output;
-
-  fetch(request)
-    .then(function(response) {
-      return response.json();
-    })
-    .catch((error) => {
-      return false;
-  });
-}
-
 function battle(...users) {
-  let gitUsers = [];
   users.forEach((user) => {
-    gitUsers.push(getGitInformation(user));
+    let request = `https://api.github.com/users/${user}`;
+    fetch(request)
+      .then(function(response) {
+        return response.json();
+      })
+      .catch((error) => {
+        return false;
+      });
   }, 0);
-  return gitUsers;
 }
-<<<<<<< HEAD
 
 battle('douglasdemoura', 'luizramos19', 'willrockies');
-=======
-  
-connectApi();
->>>>>>> 2245bd3b74fe3278735aa9674c5fe58ed285983f
