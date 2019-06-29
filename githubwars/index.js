a96436fb65863fee2a5a4d636ec7242dbdab5a3b
Douglas Moura <douglas.ademoura@gmail.com>
2019-06-29 17:10:49 -0300

    Atualização

--- githubwars/index.js
+++ githubwars/index.js
@@ -3,7 +3,8 @@ let button = document.getElementById('battle');
 button.addEventListener('click', () => {
   let user1 = document.getElementById('user-1').value;
   let user2 = document.getElementById('user-2').value;
-  battle(user1, user2);
+  console.log(user1);
+  //battle(user1, user2);
 });
 
 function battle(...users) {
@@ -17,11 +18,19 @@ function battle(...users) {
 function points(user) {
   let data = makeRequest(`https://api.github.com/users/${user}`);
   let stars = makeRequest(`https://api.github.com/users/${user}/starred`).length;
-  return data.public_repos * 20 + data.followers * 10 + data.following * 5 + stars * 10 + data.public_gists * 5;
+  Promise.all([data, stars]).then(function(data) {
+    let bonus = 0;
+
+    if (null !== data[0].bio) {
+      bonus = 100
+    }
+
+    return bonus + data[0].public_repos * 20 + data[0].followers * 10 + data[0].following * 5 + data[1] * 10 + data[0].public_gists * 5;
+  });
 }
 
 function makeRequest(url) {
-  fetch(url)
+  return fetch(url)
     .then(function(response) {
       return response.json();
     })
