17d60bb25c113a85950714cf432840a1bc49a294
Douglas Moura <douglas.ademoura@gmail.com>
2019-06-29 16:22:18 -0300

    Atualização

--- githubwars/index.js
+++ githubwars/index.js
@@ -1,14 +1,21 @@
+let button = document.getElementById('battle');
+
+button.addEventListener('click', () => {
+  let user1 = document.getElementById('user-1').value;
+  let user2 = document.getElementById('user-2').value;
+  battle(user1, user2);
+});
+
 function battle(...users) {
   users.forEach((user) => {
     let request = `https://api.github.com/users/${user}`;
     fetch(request)
       .then(function(response) {
-        return response.json();
+        data = response.json();
       })
       .catch((error) => {
         return false;
       });
   }, 0);
 }
-
-battle('douglasdemoura', 'luizramos19', 'willrockies');
+//battle('douglasdemoura', 'luizramos19', 'willrockies');
